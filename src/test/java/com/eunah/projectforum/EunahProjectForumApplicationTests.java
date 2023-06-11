8216250f2013042885eeb361dabbe94f1e33c58a
smile.loopys2 <smile.loopys2@gmail.com>
2023-06-11 17:10:08 +0900

    #6 - Starting spring boot project
    Initializing spring boot 
    Initializing technology includes: 
    
    
    * spring web
    * actuator
    * dev tools
    * lombok

---
+++ src/test/java/com/eunah/projectforum/EunahProjectForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunah.projectforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EunahProjectForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
