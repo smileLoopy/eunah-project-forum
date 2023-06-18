11b3ba2e050bb185dae86ceecec61e0f18c8e913
smile.loopys2 <smile.loopys2@gmail.com>
2023-06-18 20:37:07 +0900

    #10 - Spring Data JPA properties and settings
    * Added 'jpaConfig' to use jpa auditing function.
    * Added boot properties for connecting to db and for development convenience.
    * TODO: Exposed the DB password, need to be fixed later on.

---
+++ src/main/java/com/eunah/projectforum/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.eunah.projectforum.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("eunahkim"); //TODO: Need to be modified when adding Spring security Authentication function
+    }
+}
