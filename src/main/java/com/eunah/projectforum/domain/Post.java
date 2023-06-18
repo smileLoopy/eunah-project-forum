9359af97b773a00c288a1abbe244e47bb77112ee
smile.loopys2 <smile.loopys2@gmail.com>
2023-06-18 20:42:04 +0900

    #10 - Converted the domain codes to entity
    1. Desiged to be: Domain class == 1 Table .
    constructors, 'equals()', 'hashcode()' etc are designed in detail according to domain context.

--- src/main/java/com/eunah/projectforum/domain/Post.java
+++ src/main/java/com/eunah/projectforum/domain/Post.java
@@ -1,16 +1,72 @@
 package com.eunah.projectforum.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private  Long id;
-    private  String title;
-    private String content;
-    private  String hashtag;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
 
+    @Setter @Column(nullable = false) private  String title;
+    @Setter @Column(nullable = false, length = 10000) private String content;
+    // default is nullable so no need to set column nullable true
+    @Setter private  String hashtag;
+
+    //Relationship mapping
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private final Set<PostComment> postComments = new LinkedHashSet<>();
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
+
+    protected Post() {}
+    private Post(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+    public static Post of(String title, String content, String hashtag) {
+        return new Post(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Post post)) return false;
+        return id != null && id.equals(post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
