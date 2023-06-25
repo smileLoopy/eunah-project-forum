ad92a353f40dcac5162f03c1e208a42e839637a7
smile.loopys2 <smile.loopys2@gmail.com>
2023-06-25 14:29:08 +0900

    #1- Add api search option by using Querydsl
    Simply made it by using 
    QuerydslPredicateExecutor,
    QuerydslBinderCustomizer

--- src/main/java/com/eunah/projectforum/repository/PostCommentRepository.java
+++ src/main/java/com/eunah/projectforum/repository/PostCommentRepository.java
@@ -1,9 +1,28 @@
 package com.eunah.projectforum.repository;
 
 import com.eunah.projectforum.domain.PostComment;
+import com.eunah.projectforum.domain.QPostComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface PostCommentRepository extends JpaRepository<PostComment,Long> {
+public interface PostCommentRepository extends
+        JpaRepository<PostComment,Long>,
+        QuerydslPredicateExecutor<PostComment>,
+        QuerydslBinderCustomizer<QPostComment> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QPostComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first((StringExpression::containsIgnoreCase));
+        bindings.bind(root.createdAt).first((DateTimeExpression::eq));
+        bindings.bind(root.createdBy).first((StringExpression::containsIgnoreCase));
+    }
 }
+
