c694b61e2be1146f65e45683caf706959b6b4e41
smile.loopys2 <smile.loopys2@gmail.com>
2023-06-25 11:58:20 +0900

    #1- Data rest settings
    Setting json api of post, and postcomment to be automatically restful

--- src/main/java/com/eunah/projectforum/repository/PostRepository.java
+++ src/main/java/com/eunah/projectforum/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.eunah.projectforum.repository;
 
 import com.eunah.projectforum.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
